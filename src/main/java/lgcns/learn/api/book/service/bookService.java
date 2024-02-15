e55a49a01387173fd3c41e8ef7228658483c4323
kongjonghun <rhd0503@naver.com>
2024-02-16 00:19:57 +0900

    init commit

---
+++ src/main/java/lgcns/learn/api/book/service/bookService.java
@@ -0,0 +1,10 @@
+package lgcns.learn.api.book.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class bookService {
+
+}
---
+++ src/main/java/lgcns/learn/api/book/vo/book.java
@@ -0,0 +1,12 @@
+package lgcns.learn.api.book.vo;
+
+import lombok.*;
+
+@Data
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class book {
+
+}
