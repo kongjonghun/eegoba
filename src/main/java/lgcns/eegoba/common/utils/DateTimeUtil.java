cd2f8d9c6bd8e1dcd8e0cd58508bc3bbbf11abca
gialee <gia.lee@gmail.com>
2024-02-29 14:02:37 +0900

    [Update] DateTimeUtil 수정 - 기존 중복되는 체이닝 메서드 삭제 - 타입 변경 (LocalDateTime -> ZonedDateTime)

--- src/main/java/lgcns/eegoba/common/utils/DateTimeUtil.java
+++ src/main/java/lgcns/eegoba/common/utils/DateTimeUtil.java
@@ -1,19 +1,18 @@
 package lgcns.eegoba.common.utils;
 
-import lombok.experimental.UtilityClass;
-
-import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
+import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class DateTimeUtil {
 
-    private static final ZoneId SEOUL_ZONE =  ZoneId.of("Asia/Seoul");
+  private static final ZoneId SEOUL_ZONE = ZoneId.of("Asia/Seoul");
 
-    public Date getCurrentSeoulDateTime() {
-        LocalDateTime seoulLocalDateTime = LocalDateTime.now(SEOUL_ZONE);
+  public Date getSeoulDateTime() {
+    ZonedDateTime nowSeoul = ZonedDateTime.now(SEOUL_ZONE);
 
-        return Date.from(seoulLocalDateTime.atZone(SEOUL_ZONE).toInstant());
-    }
+    return Date.from(nowSeoul.toInstant());
+  }
 }
